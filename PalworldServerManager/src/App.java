242c250ddb001e86259bdb40dba1b2ec603ec322
eefernet <ethanryanjamieson@gmail.com>
2024-01-31 01:02:53 -0700

    Massive Changes to parse algo

--- PalworldServerManager/src/App.java
+++ PalworldServerManager/src/App.java
@@ -5,6 +5,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
@@ -15,6 +16,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.lang.reflect.Array;
 import java.net.URL;
 import java.nio.file.NoSuchFileException;
@@ -26,6 +28,7 @@ public class App {
     static Scanner scnr = new Scanner(System.in);
     static File file;
     static JFrame frame;
+    static String rawString="";
     static ArrayList<String> settings = new ArrayList<String>();
     static ArrayList<Settings> settingsObjects = new ArrayList<Settings>();
 
@@ -52,9 +55,39 @@ public class App {
             URL path = App.class.getResource("settings.txt");
             file = new File(path.getFile());
             Scanner fileReader = new Scanner(file);
-            while(fileReader.hasNextLine()){
-                settings.add(fileReader.nextLine());
+            
+
+            while (fileReader.hasNextLine()) {
+                String setting = fileReader.nextLine();
+                rawString+= setting;
+                
             }
+            System.out.println(rawString);
+
+            rawString = extractSubstringBetweenParentheses(rawString, "(", ")");
+            System.out.println("updated RAW STRING : "+rawString);
+            
+            fileReader = new Scanner(rawString).useDelimiter(",");
+            while(fileReader.hasNext()){
+                String string = fileReader.next();
+                System.out.println("File reader has parsed : " + string);
+                settings.add(string);
+            }
+
+            for (String string : settings) {
+                String[] values = string.split("=");
+                Settings settingsObject = new Settings(values[0], values[1], Settings.SettingType.Other);
+                settingsObjects.add(settingsObject);
+            }
+
+            System.out.println("COUNT OF SETTINGS OBJECTS ARRAY : "+ settingsObjects.size());
+            for (Settings objec : settingsObjects) {
+                System.out.println("Key : " + objec.getSettingName() + "   value: " + objec.getSettingValue());
+            }
+
+
+
+            System.out.println("SETUPDATA METHOD : "+settings.size());
             fileReader.close();
             fileLoaded = true;
             settings.remove(0);
@@ -72,7 +105,7 @@ public class App {
         tabbedPane.setBounds(0, 0, 480, 620);
 
         JPanelViewController serverPanel = new JPanelViewController(JPanelViewController.PanelType.ServerSettings);
-        JPanelViewController palsPanel = new JPanelViewController(JPanelViewController.PanelType.Palsettings);
+        JPanelViewController helpPanel = new JPanelViewController(JPanelViewController.PanelType.Palsettings);
 
         JPanel saveButtonPanel = new JPanel();
         saveButtonPanel.setBounds(tabbedPane.getWidth(), tabbedPane.getHeight(), frame.getWidth(), 100);
@@ -92,7 +125,7 @@ public class App {
         });
 
         tabbedPane.add("Server Settings", serverPanel);
-        tabbedPane.add("Pal Settings", palsPanel);
+        tabbedPane.add("Help", helpPanel);
         
         
         frame.setSize(480,720);
@@ -106,30 +139,29 @@ public class App {
     private static void processObjects(){
         if(fileLoaded){
             for(String setting : settings){
-                String[] settingArray = setting.split("=");
+                System.out.println(setting);
+                String[] settingArray = setting.trim().split("=");
                 
-                
-                if(settingArray[0].toLowerCase().contains("difficulty") || setting.toLowerCase().contains("maxplayers") || setting.toLowerCase().contains("rcon")|| setting.toLowerCase().contains("guild") || setting.toLowerCase().contains("serverame") || setting.toLowerCase().contains("public")|| setting.toLowerCase().contains("port") || setting.toLowerCase().contains("serverpassword")|| setting.toLowerCase().contains("admin")){
-                    Settings newSetting = new Settings(settingArray[0], settingArray[1], Settings.SettingType.ServerSettings);
-                    settingsObjects.add(newSetting);
-                }
-
-                if(settingArray[0].toLowerCase().contains("pal")){
-                    Settings newSetting = new Settings(settingArray[0], settingArray[1], Settings.SettingType.PalSettings);
-                    settingsObjects.add(newSetting);
-                }
-
-                else{
-                    Settings newSetting = new Settings(settingArray[0], settingArray[1], Settings.SettingType.Other);
-                    settingsObjects.add(newSetting);
-                }
-
-                System.out.println("Count : " + settingsObjects.size());
+                Settings newSetting = new Settings(settingArray[0], settingArray[1], Settings.SettingType.ServerSettings);
+                settingsObjects.add(newSetting);
+                System.out.println(settingsObjects.size());
             }
 
-            for(Settings settingObject : settingsObjects){
-                System.out.println(settingObject.getSettingName() + " " + settingObject.getSettingValue() + " " + settingObject.getType());
+            for (Settings setting: settingsObjects) {
+                System.out.println(setting.getSettingName() + " ::: " + setting.getSettingValue());                
             }
+            
+        }
+    }
+
+    public static String extractSubstringBetweenParentheses(String inputString, String index1, String index2) {
+        int startIndex = inputString.indexOf(index1);
+        int endIndex = inputString.indexOf(index2);
+
+        if (startIndex != -1 && endIndex != -1 && startIndex < endIndex) {
+            return inputString.substring(startIndex + 1, endIndex);
+        } else {
+            return null;
         }
     }
 
--- PalworldServerManager/src/JPanelViewController.java
+++ PalworldServerManager/src/JPanelViewController.java
@@ -1,3 +1,7 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,11 +31,46 @@ public class JPanelViewController extends JPanel{
     public void setUpPanel(PanelType type){
         switch(type){
             case ServerSettings:
-            this.setLayout(new FlowLayout());
-            for(int i = 0; i < settings.size(); i++){
+            this.setLayout(new BorderLayout());
+
+            //wrapper panel to throw into the scroll view
+            JPanel wrapperPanel = new JPanel();
+            wrapperPanel.setLayout(new BoxLayout(wrapperPanel, BoxLayout.Y_AXIS));
+            
+            //Add all the settings in one scroll pane
+            for (Settings settings : App.settingsObjects) {
+                //make a setting pannel
+                JPanel settingPanel = new JPanel();
+                settingPanel.setBounds(this.getWidth(), this.getHeight(), this.getWidth(), 100);
+                //Add textPane setting description
+                JTextPane textPane = new JTextPane();
+                textPane.setBounds(0,0,350,40);
+                textPane.setAlignmentX(RIGHT_ALIGNMENT);
+                textPane.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+                textPane.setText(settings.getSettingName());
+
+                //Add the textField
+                JTextField textField = new JTextField();
+                textField.setText(settings.getSettingValue());
+                textField.setAlignmentX(RIGHT_ALIGNMENT);
                 
+                //add the fields to the setting panel
+                settingPanel.add(textPane);
+                settingPanel.add(textField);
+
+                //Set layout and ad it to the wrapper panel
+                //TODO: change later to a different layout that makes sense so they are all aligned.
+                settingPanel.setLayout(new FlowLayout());
+                wrapperPanel.add(settingPanel);   
             }
 
+            //Create a scroll panel with a child of wrapperPanel
+            JScrollPane scrollPanel = new JScrollPane(wrapperPanel);
+            //set scroll bar policy
+            scrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+            //Add scroll bar
+            add(scrollPanel);
+            
                 break;
             case Palsettings:
 
